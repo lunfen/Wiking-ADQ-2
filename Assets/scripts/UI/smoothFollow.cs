588504edb61854f0bf7e5a3eb455331f805b7e72
Alexey Starchenko <lunfen@yandex.ru>
2015-08-12 18:38:52 +0300

    12.08 18:38
    black screen added

--- Assets/scripts/UI/smoothFollow.cs
+++ Assets/scripts/UI/smoothFollow.cs
@@ -11,14 +11,30 @@ public class smoothFollow : MonoBehaviour
 	int index;
 	float bestDistance;
 	Transform player;
+	RectTransform levelMask;
+	bool switchDampTime;
 
 	void Awake ()
 	{
 		player = GameObject.Find("character").transform;
+		levelMask = GameObject.Find("LevelMask").GetComponent<RectTransform>();
+
+		//fix respawn bug
+		player.gameObject.GetComponent<charController>().moveRight();
+		player.gameObject.GetComponent<charController>().moveNone();
 	}
 	
 	void Update () 
 	{
+		// level mask anim
+		if (player.gameObject.GetComponent<charController>().deadTimer == 0)
+		{
+			if (levelMask.sizeDelta.x > 0)
+				levelMask.sizeDelta = new Vector2(levelMask.sizeDelta.x-50, levelMask.sizeDelta.x-50);
+		}
+		else
+			levelMask.sizeDelta = new Vector2(levelMask.sizeDelta.x+50, levelMask.sizeDelta.x+50);
+
 		// start
 		bestDistance = Vector2.Distance(player.position, points[0].position);
 		index = 0;
@@ -37,7 +53,19 @@ public class smoothFollow : MonoBehaviour
 		target = points[index];
 
 		if (target.GetComponent<CircleCollider2D>().radius < bestDistance)
+		{
 			target = player.transform;
+			if (dampTime > 2f)
+				switchDampTime = true;
+		}
+		else if (dampTime < 2)
+			dampTime += 0.05f;
+
+		if (switchDampTime)
+			if (dampTime > 0.2f)
+				dampTime -= 0.05f;
+			else
+				switchDampTime = false;
 
 		// smooth follow apply
 		if (target)
@@ -54,5 +82,10 @@ public class smoothFollow : MonoBehaviour
 		else if (player.GetComponent<charController>().deadTimer > 0)
 			player.GetComponent<charController>().deadTimer += Time.deltaTime;
 
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(150,20,100,100), "damp time " + dampTime);
 	}
 }
